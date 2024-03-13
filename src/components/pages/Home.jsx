f6fff85d7dbf8b1258ac7495967c83fdfc24a7ac
HuskedX <140060664+HuskedX@users.noreply.github.com>
2024-03-13 16:52:25 +0000

    new file:   node_modules/.cache/babel-loader/2f8fb8651dac8c763ab45479a4493f0fc63d05183164ba7e2a19f987e43c6504.json new file:   node_modules/.cache/babel-loader/42e1d972769e01d9151e7361fed8de4078d956da573c53c2b2acff83f4a95e22.json new file:   node_modules/.cache/babel-loader/4878059b1511ecc0e0284d243c70054b4342bd65d768788c798ef51d20f75bf7.json new file:   node_modules/.cache/babel-loader/4d86d5ec662e5eb4273fe5d22d1b85c1366b7510f2340660de6182510c255059.json new file:   node_modules/.cache/babel-loader/9d643cd469553fa837a828c3268c77a9379a0c909ca96f5c3dbebe5d284ef5a1.json new file:   node_modules/.cache/babel-loader/a2d1c0e1cf78a6921c7b5a3a6818ae92f0405504cac228b019e062b2d73f5f6c.json new file:   node_modules/.cache/babel-loader/b49bbda31de7976cf3a04f3789a6fd2f3141026da648432db208759e9dbc9759.json new file:   node_modules/.cache/babel-loader/c0ac04e18d4e1d4c3b2fa992ecd1e04109038a7e53201c799f3ee8e957635f1a.json

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Navbar from "../Navbar";
+import thumbsUpGif from '../image/thumbs-up.gif'; // Import the thumbs up GIF
+import logo from '../image/CollaFilter Logo.jpg'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <h1>Home</h1>
+
+      {/* Introduction Section */}
+      <div className="introduction-section">
+        <h2>Welcome to [Your Platform Name]</h2>
+        <p>Discover personalized product recommendations and enhance your shopping experience with us. Our platform uses collaborative filtering to suggest products tailored to your interests and preferences.</p>
+        <img src={thumbsUpGif} alt="Thumbs Up GIF" />
+      </div>
+
+      {/* Showcase Section */}
+      <div className="showcase-section">
+        {/* Logo Slider */}
+        <h3>Our Blogshop Partners</h3>
+        <div className="logo-slider">
+          <img src={logo} alt="CollaFilter Logo" className='logo' />
+          <img src={logo} alt="CollaFilter Logo" className='logo' />
+          <img src={logo} alt="CollaFilter Logo" className='logo' />
+          {/* Add more logo images as needed */}
+        </div>
+      </div>
+
+      {/* Bookmark and Recommendation Section */}
+      <div className="bookmark-section">
+        <h3>Bookmark Your Favorites</h3>
+        <p>Receive updated product recommendations based on your preferences and interactions.</p>
+        <img src={thumbsUpGif} alt="Thumbs Up GIF" />
+      </div>
     </div>
   );
 };
