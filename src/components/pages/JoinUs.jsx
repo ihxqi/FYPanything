a17650facff7962d8288e782d870df1b2ed636c1
HuskedX <140060664+HuskedX@users.noreply.github.com>
2024-03-14 14:27:15 +0000

    main

--- src/components/pages/JoinUs.jsx
+++ src/components/pages/JoinUs.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "../Navbar";
 
@@ -13,19 +12,3 @@ const JoinUs = () => {
 };
 
 export default JoinUs;
-=======
-import React from "react";
-import Navbar from "../Navbar";
-
-
-const JoinUs = () => {  
-  return (
-    <div>
-      <Navbar />
-      <h1>Join Us!</h1>
-    </div>
-  );
-};
-
-export default JoinUs;
->>>>>>> f6fff85d7dbf8b1258ac7495967c83fdfc24a7ac
